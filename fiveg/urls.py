ed2832e620d7f715cbc0d02280e52baacfd5bcc6
Webmaster <macshvot@gmail.com>
2021-06-18 16:13:11 -0300

    Added an About Page to Templates

--- fiveg/urls.py
+++ fiveg/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
+from .views import about
 
 urlpatterns = [
     path('home/', home),
+    path('about/', about),
 ]
--- fiveg/views.py
+++ fiveg/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, template_name="fiveg/home.html")
+
+
+def about(request):
+    return render(request, template_name="fiveg/about.html")
